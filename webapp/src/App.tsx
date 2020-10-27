e39419fa2fc8a3e1d01e0bbadcb977417e04ec84
Willian Soares <dasilva.williansoares@gmail.com>
2020-10-27 15:15:13 -0300

    Versão web da aplicação

---
+++ webapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';  
+import './styles/global.css'; 
+import 'leaflet/dist/leaflet.css'
+
+
+import Routes from './routes';
+//import { resourceUsage } from 'process';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
